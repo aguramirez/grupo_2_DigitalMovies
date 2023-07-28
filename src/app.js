7055a315983473536a8aa45f3a5b04ec25ceb4fb
aguramirez <agustinramirez58@gmail.com>
2023-07-28 16:21:46 -0300

    agrego login y register, arreglo navbar global

--- src/app.js
+++ src/app.js
@@ -12,3 +12,9 @@ app.get('/', (req, res) => {
 app.get('/compras', (req, res) => {
     res.sendFile(path.resolve(__dirname, "./views/productCart.html"))
 });
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+});
