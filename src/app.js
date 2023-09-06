04d170181dfa9dbfe24b996b0f6fc989c550db49
aguramirez <agustinramirez58@gmail.com>
2023-09-06 01:12:27 -0300

    crud completo funciona, (el read en productDetail da error pero funciona)

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ const express = require('express');
 const app = express();
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes');
+const methodOverride = require('method-override'); // Para poder usar PUT y DELETE
 
 app.use(express.static('public'));
 
 //para que funcione el post en los form
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(methodOverride('_method')); // Para poder usar PUT y DELETE
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
--- src/routes/productsRoutes.js
+++ src/routes/productsRoutes.js
@@ -8,7 +8,7 @@ const storage = multer.diskStorage({
         cb(null, path.join(__dirname, '../../public/images/'))
     },
     filename: function (req, file, cb) {
-        console.log(file);
+        // console.log(file);
         cb(null, "pelicula-" + Date.now() + path.extname(file.originalname))
     },
 });
@@ -18,7 +18,13 @@ const upload = multer({storage: storage});
 const productsController = require('../controllers/productsController');
 
 router.get('/detail/:id/', productsController.detail)
+
 router.get('/create', productsController.create);
 router.post('/create', upload.single("imagen"), productsController.postCreate);
+
+router.get('/:id/edit', productsController.edit);
+router.put('/:id', upload.single("imagen"), productsController.putEdit);
+
+router.delete('/:id', productsController.delete);
 
 module.exports= router;
